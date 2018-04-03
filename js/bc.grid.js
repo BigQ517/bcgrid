f5d063e97b7d95c6113e746a0971c97a41571a6c
bigq517 <bigq517@qq.com>
2018-04-03 21:10:38 +0800

    build grid css

---
+++ js/bc.grid.js
@@ -0,0 +1,10 @@
+/**
+ * File:bc.grid.js
+ * Youth is just a section of unoptimized code!
+ * -------------------------------------------------------------------------
+ * Created by BigQ on 2018/4/3.
+ *--------------------------------------------------------------------------
+ * This technical information is confidential information,
+ * without my written,permission is prohibited for commercial purposes.　
+ *--------------------------------------------------------------------------
+ */
