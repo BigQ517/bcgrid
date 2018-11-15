f8e6c5db791c2d475e7b4a3fe5e3edff2fec1e3c
bigq517 <bigq517@qq.com>
2018-11-15 21:45:11 +0800

    加了好多东西，包括(#4)

--- dist/lang/zh.js
+++ dist/lang/zh.js
@@ -7,7 +7,14 @@
  */
 BCGrid.addLang('zh',{
     'Serial':'序号',
+    'ON':'开',
+    'OFF':'关',
     'Loading':'数据加载中...',
+    'Edit':'编辑',
+    'Delete':'删除',
+    'Add':'添加',
+    'Remove':'移除',
+    'Detail':'明细',
     'Per page':'每页',
     'Records':'记录',
      'First':'首页',
